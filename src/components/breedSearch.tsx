45490b4d48971f2243b8b4f916d6a6079949b7bb
IciIcifur <iciicifur@yandex.ru>
2025-04-11 11:50:56 +0300

    Tiny adaptivity fixes

--- src/components/breedSearch.tsx
+++ src/components/breedSearch.tsx
@@ -8,14 +8,14 @@ export default function BreedSearch({
   setSearchValue: any;
 }) {
   return (
-    <div className="flex w-full justify-end">
-      <div className=" flex flex-col gap-1">
+    <div className="flex w-full lg:justify-end justify-start">
+      <div className=" flex flex-col gap-1 w-96">
         <p className="text-neutral-500 ml-1">Search for a breed</p>
         <div className="flex items-center">
           <input
             value={searchValue}
             onChange={(event) => setSearchValue(event.target.value)}
-            className="p-2 bg-neutral-800 border-[1.5px] transition-all duration-300 border-orange-500 rounded-xl w-96
+            className="p-2 bg-neutral-800 border-[1.5px] transition-all duration-300 border-orange-500 rounded-xl w-full
         focus:outline-none focus:border-yellow-500"
             placeholder="Start typing a breed name..."
             type="text"
--- src/components/doggyHeader.tsx
+++ src/components/doggyHeader.tsx
@@ -2,8 +2,8 @@ import logo from "../assets/logo.png";
 
 export default function DoggyHeader() {
   return (
-    <header className="flex flex-wrap w-full h-fit gap-2 items-center">
-      <img className="w-80" src={logo} alt="Doggy logo" />
+    <header className="flex flex-wrap w-full h-fit gap-2 items-center sm:justify-start justify-center">
+      <img className="w-64 max-w-80 md:w-80" src={logo} alt="Doggy logo" />
       <div className="flex flex-col gap-4">
         <h1
           className="text-7xl md:text-8xl h-fit font-semibold pb-4 bg-gradient-to-r from-orange-500 to-yellow-400
